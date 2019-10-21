c67d1d13ca255b3144211e0a67fb6e2aca979623
Yudhi wibowo7 <yudhisatriowibowo@gmail.com>
2019-10-21 21:32:05 +0700

    desktop4

---
+++ yudhi/dekstop4.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DEKSTOP 4</title>
+</head>
+
+<style>
+.navbar {
+    overflow: hidden;
+    background-color : white;
+    font-family: Arial;
+  }
+  
+  /* Links inside the navbar */
+  .navbar a {
+    float: right;
+    font-size: 16px;
+    color : black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  
+
+  
+  /* Add a red background color to navbar links on hover */
+  .navbar a:hover, .dropdown:hover .dropbtn {
+    background-color: rgb(0, 153, 255);
+  }
+
+
+
+
+</style>
+
+<div class="navbar">
+    
+    <a href="#">About Us</a>
+    <a href="#">Cari Lowongan</a>
+    <a href="#">Login</a>  
+    
+  </div>
+<body>
+
+<div class="image">
+                <img src="1.png" alt="1" class="center" width=4% ><b style="font-size: 20px;">LockerNich</b>
+</div>
+
+
+ <h4>DESKRIPSI PEKERJAAN</h4>
+ <ul>
+ <li>Max 30 years old</li>
+ <li>Candidate must posseas at last ........</li>
+ <li>at least 1 years .....</li>
+ <li>required skill....</li>
+ <li>preferably less than 1 years experience ....</li>
+</ul>
+
+<h3>Description</h3>
+<ul>
+	<li>To sell the produck or service corporate customers</li>
+	<li>perform the administration procces ..</li>
+	<li>keeping a good relationship.....</li>
+	<li>make a report</li>
+</ul>
+
+<h3>Placement</h3>
+<P>CBN - Surabaya <br>
+Ruko Embong Kemiri Square<br>
+Jl.Embong Kemiri NP.2E<br>
+Emobong Kaliasin - Genteng<br>
+Surabaya, 60271</P>
+
+<h4>INFORMASI PERUSAAHAAN</h4>
+<h2>Urgently Required !!</h2>
+<p>Vision: <br>
+"To be leader in the information and communication technology industry by providing .....</p>
+
+
+<p>Mission :<br>
+with the objective of the building the internet community in indonesia ...</p>
+<ul>
+	<li>To provide solution in telecomunication ...</li>
+	<li>To provide an excelent..</li>
+	<li>To maintain integrity....</li>
+
+</ul>
+
+
+
+
+
+
+
+
+
+
+<div>
+    <img src="5.png" alt="1" class="center" width=100% >
+ </div>
+
+
+
+</body>
+</html>
