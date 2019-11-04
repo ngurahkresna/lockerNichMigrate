c4669342b691968469c8142e6f023ffdff8f5663
ngurahkresna <ngurahkresna99@gmail.com>
2019-11-04 21:13:45 +0700

    register belum jadi

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/register',  'RegisterController@registrationForm');
+Route::post('/register',  'RegisterController@registerUser');
+
 Route::get('/home', 'HomeController@index')->name('home');
