e0ebfdfca846da1385747e0fc218f4fc8788dd7a
unknown <devitaawulandari@gmail.com>
2019-11-18 21:20:16 +0700

    View pada Article

--- views/article.php
+++ views/article.php
@@ -1,1 +1,55 @@
-<!--Devita-->
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>Daftar Lowongan</title>
+</head>
+<body>
+<?php include_once('navbar.php') ?>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-10  mr-auto ml-auto">
+            <div class="d-flex bd-highlight shadow-sm p-2 mb-3 bg-white rounded">
+                <div class="p-2 w-100 bd-highlight">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-1 p-3">
+                                <img class="avatar1" src="assets/img/avatar.jpg" alt="ava">
+                            </div>
+                            <div class="col-8">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <h5>Nama User</h5>
+                                        </div>
+                                        <div class="col-12">
+                                            <p>Tanggal</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr style="margin: 4px">
+                    <h3 class="mt-3"><b>Nama Artikel</b></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi viverra libero ut lacinia
+                        suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                        inceptos
+                        himenaeos. Ut nec molestie dolor. Proin aliquam bibendum pretium. Mauris commodo nec
+                        nulla
+                        ut
+                        placerat. Praesent ornare leo dolor, quis imperdiet est luctus non. Duis viverra
+                        lobortis
+                        augue,
+                        ut blandit eros congue ac.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
